85a6a324a1970092f5c688aa31ffc23d2b312c41
Pamplemousse <xavier.maso@net-c.com>
2015-01-02 15:03:01 +1300

    test tags' and series' services

--- test/spec/services/series.js
+++ test/spec/services/series.js
@@ -6,13 +6,34 @@ describe('Service: series', function () {
   beforeEach(module('libraryboxApp'));
 
   // instantiate service
-  var series;
-  beforeEach(inject(function (Series) {
-    series = Series;
+  var series, httpBackend;
+  beforeEach(inject(function (_Series_, $httpBackend) {
+    series = _Series_;
+    httpBackend = $httpBackend;
+    httpBackend.whenGET('/data/dump-series.json').respond([
+      {"id":1,"name":"Bas de Cuir","sort":"Bas de Cuir"},
+      {"id":2,"name":"Arsène Lupin, gentleman cambrioleur","sort":"Arsène Lupin, gentleman cambrioleur"},
+      {"id":3,"name":"Mille et une nuits","sort":"Mille et une nuits"}
+    ]);
   }));
 
   it('should do something', function () {
     expect(!!series).toBe(true);
+  });
+
+  it('is able to return a list of series', function() {
+    series.all().then(function (returnedSeries) {
+      expect(returnedSeries.length).toEqual(3);
+    });
+    httpBackend.flush();
+  });
+
+  it('is able to find an serie with his id', function() {
+    series.find(2).then(function(returnedSerie) {
+      expect(returnedSerie.id).toEqual(2);
+      expect(returnedSerie.name).toEqual("Arsène Lupin, gentleman cambrioleur");
+    });
+    httpBackend.flush();
   });
 
 });
--- test/spec/services/tags.js
+++ test/spec/services/tags.js
@@ -6,13 +6,36 @@ describe('Service: tags', function () {
   beforeEach(module('libraryboxApp'));
 
   // instantiate service
-  var tags;
-  beforeEach(inject(function (Tags) {
-    tags = Tags;
+  var tags, httpBackend;
+  beforeEach(inject(function (_Tags_, $httpBackend) {
+    tags = _Tags_;
+    httpBackend = $httpBackend;
+    httpBackend.whenGET('/data/dump-tags.json').respond([
+      {"id":55,"name":"Danemark"},
+      {"id":56,"name":"Allemagne"},
+      {"id":57,"name":"Italie"},
+      {"id":58,"name":"Chine"},
+      {"id":60,"name":"Littérature sentimentale"}
+    ]);
   }));
 
   it('should do something', function () {
     expect(!!tags).toBe(true);
+  });
+
+  it('is able to return a list of tags', function() {
+    tags.all().then(function (returnedTags) {
+      expect(returnedTags.length).toEqual(5);
+    });
+    httpBackend.flush();
+  });
+
+  it('is able to find an tag with his id', function() {
+    tags.find(56).then(function(returnedTag) {
+      expect(returnedTag.id).toEqual(56);
+      expect(returnedTag.name).toEqual("Allemagne");
+    });
+    httpBackend.flush();
   });
 
 });
