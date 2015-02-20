0f034073edc008f1bc24338a99b4ee1770df5667
Pamplemousse <xavier.maso@net-c.com>
2015-02-20 16:57:13 +1300

    begin details UI

--- app/scripts/controllers/authors.js
+++ app/scripts/controllers/authors.js
@@ -24,10 +24,15 @@ angular.module('libraryboxApp')
 
 
 angular.module('libraryboxApp')
-.controller('AuthorDetailCtrl', ['$scope', '$routeParams', 'Authors', function($scope, $routeParams, Authors) {
+.controller('AuthorDetailCtrl', ['$scope', '$routeParams', 'Authors', 'Books', function($scope, $routeParams, Authors, Books) {
 
     Authors.find($routeParams.authorId).then(function(data) {
       $scope.author = data;
+    });
+
+    // just stupidly retrive 6 books to design the view
+    Books.all().then(function(data) {
+      $scope.books = data.slice(0,6);
     });
 
 }]);
--- app/scripts/controllers/images.js
+++ app/scripts/controllers/images.js
@@ -9,9 +9,5 @@
  */
 angular.module('libraryboxApp')
   .controller('ImagesCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
   });
